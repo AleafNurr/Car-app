2ffa5ee21e0293a9f972000aafb3b2815aacc7b3
AleafNurr <alef.nurrokh@gmail.com>
2023-06-28 17:08:01 +1200

    Added Car + Car Controls

---
+++ controls.js
@@ -0,0 +1,46 @@
+class Controls{
+    constructor(){
+        this.foward = false;
+        this.left = false;
+        this.right = false;
+        this.reversed = false;
+
+        this.#addListeners();
+    }
+
+    // # is a private method
+    #addListeners(){
+        document.onkeydown = (e) => {
+            switch(e.key){
+                case 'ArrowUp':
+                    this.foward = true;
+                    break;
+                case 'ArrowLeft':
+                    this.left = true;
+                    break;
+                case 'ArrowRight':
+                    this.right = true;
+                    break;
+                case 'ArrowDown':
+                    this.reversed = true;
+                    break;
+            }
+        }
+        document.onkeyup = (e) => {
+            switch(e.key){
+                case 'ArrowUp':
+                    this.foward = false;
+                    break;
+                case 'ArrowLeft':
+                    this.left = false;
+                    break;
+                case 'ArrowRight':
+                    this.right = false;
+                    break;
+                case 'ArrowDown':
+                    this.reversed = false;
+                    break;
+            }
+        }
+    }
+}
